bf56d3c77e4dc70cc12668fa4da8857dc4537992
Dimitar Dimitrov <dimitarbd@abv.bg>
2025-04-08 22:42:36 +0300

    Update Contact.jsx

--- Client/src/Components/contact/Contact.jsx
+++ Client/src/Components/contact/Contact.jsx
@@ -22,8 +22,8 @@ export default function Contact() {
               <GoogleMap />
           </div>
           <div className="container-fluid">
-              <div className="row">
-                  <div className="col-lg-5 offset-lg-1 col-md-12 order-1 order-lg-2">
+              <div className="row justify-content-center">
+                  <div className="col-lg-5 col-md-12 order-1 order-lg-2">
                       <div className="contact-page-side-content">
                           <h3 className="contact-page-title">Contact Us</h3>
                           <p className="contact-page-message">Claritas est etiam processus dynamicus, qui sequitur
@@ -43,35 +43,6 @@ export default function Contact() {
                               <p>yourmail@domain.com</p>
                               <p>support@hastech.company</p>
                           </div>
-                      </div>
-                  </div>
-                  <div className="col-lg-6 col-md-12 order-2 order-lg-1">
-                      <div className="contact-form-content">
-                          <h3 className="contact-page-title">Tell Us Your Message</h3>
-                          <div className="contact-form">
-                              <form id="contact-form" action="http://hasthemes.com/file/mail.php" method="post">
-                                  <div className="form-group">
-                                      <label>Your Name <span className="required">*</span></label>
-                                      <input type="text" name="con_name" id="con_name" required />
-                                  </div>
-                                  <div className="form-group">
-                                      <label>Your Email <span className="required">*</span></label>
-                                      <input type="email" name="con_email" id="con_email" required />
-                                  </div>
-                                  <div className="form-group">
-                                      <label>Subject</label>
-                                      <input type="text" name="con_subject" id="con_subject" />
-                                  </div>
-                                  <div className="form-group form-group-2">
-                                      <label>Your Message</label>
-                                      <textarea name="con_message" id="con_message"></textarea>
-                                  </div>
-                                  <div className="form-group">
-                                      <button type="submit" value="submit" id="submit" className="uren-contact-form_btn" name="submit">send</button>
-                                  </div>
-                              </form>
-                          </div>
-                          <p className="form-messege"></p>
                       </div>
                   </div>
               </div>
